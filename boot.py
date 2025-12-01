272366c9eaa4ddf2862b2b60d90243ea9873d3dd
Deby <44121160+DebyGe@users.noreply.github.com>
2025-12-01 09:45:37 +0100

    Add USB drive detection in boot

---
+++ boot.py
@@ -0,0 +1,16 @@
+import board
+import digitalio
+import storage
+
+noStorage = False
+noStoragePin = digitalio.DigitalInOut(board.GP9)
+noStoragePin.direction = digitalio.Direction.INPUT
+noStoragePin.pull = digitalio.Pull.DOWN
+noStorage = noStoragePin.value
+
+if(noStorage == True):
+    print("USB drive  enabled")
+else:
+    # normal boot
+    storage.disable_usb_drive()
+    print("USB drive disable")
